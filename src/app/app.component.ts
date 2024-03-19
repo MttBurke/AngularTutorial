20f37ab0c15b1a1658e0efef5e2efc3f86a8ff36
Matthew Burke <matthew.burke@lexisnexisrisk.com>
2024-03-19 12:47:23 +0000

    routes and detail page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,15 @@
 import {Component} from '@angular/core';
 import {HomeComponent} from "./home/home.component";
+import {RouterModule} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
-  template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/favicon.ico" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
-  `,
+  imports: [
+    HomeComponent,
+    RouterModule
+  ],
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
